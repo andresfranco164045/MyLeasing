94a265081ffaa53747e3fdf3440d1aa36de2f926
ANDRES FELIPE FRANCO GUZMAN <andresfranco164045@correo.itm.edu.co>
2019-08-05 19:58:10 -0500

    Creating the First Entity

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
