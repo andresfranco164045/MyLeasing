da7ff710df79f3236a35d1062b258cc831c8ed70
ANDRES FELIPE FRANCO GUZMAN <andresfranco164045@correo.itm.edu.co>
2019-08-14 21:35:26 -0500

    Showing Properties on Owner Detail View

--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -36,7 +36,13 @@ namespace MyLeasing.Web.Controllers
             }
 
             var owner = await _dataContext.Owners
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .Include(o => o.User)
+                .Include(o => o.Properties)
+                .ThenInclude(p => p.PropertyImages)
+                .Include(o => o.Contracts)
+                .ThenInclude(c => c.Lessee)
+                .ThenInclude(l => l.User)
+                .FirstOrDefaultAsync(o => o.Id == id);
             if (owner == null)
             {
                 return NotFound();
