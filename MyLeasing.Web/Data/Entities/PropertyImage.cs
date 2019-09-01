99a77f8518fd7c0b49ba1824fd66edfabf118671
ANDRES FELIPE FRANCO GUZMAN <andresfranco164045@correo.itm.edu.co>
2019-09-01 13:35:58 -0500

    Publish on Azure and Change Path of publish en PropertyImage

--- MyLeasing.Web/Data/Entities/PropertyImage.cs
+++ MyLeasing.Web/Data/Entities/PropertyImage.cs
@@ -13,8 +13,8 @@ namespace MyLeasing.Web.Data.Entities
 
         // TODO: Change the path when publish
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
-            ? "https://TBD.azurewebsites.net/images/Properties/noImage.png"
-            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+            ? "https://myleasingfranco.azurewebsites.net/images/Properties/noImage.png"
+            : $"https://myleasingfranco.azurewebsites.net{ImageUrl.Substring(1)}";
     }
 
 }
