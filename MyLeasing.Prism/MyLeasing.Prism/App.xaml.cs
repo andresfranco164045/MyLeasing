338839473d1e8dae52bcc1898a2e2a6df1f8e732
ANDRES FELIPE FRANCO GUZMAN <andresfranco164045@correo.itm.edu.co>
2019-11-07 20:01:12 -0500

    Show Real Location of the User on the Map

--- MyLeasing.Prism/MyLeasing.Prism/App.xaml.cs
+++ MyLeasing.Prism/MyLeasing.Prism/App.xaml.cs
@@ -48,10 +48,11 @@ namespace MyLeasing.Prism
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.Register<IApiService, ApiService>();
+            containerRegistry.Register<IGeolocatorService, GeolocatorService>();
             containerRegistry.RegisterForNavigation<NavigationPage>();
             containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
             containerRegistry.RegisterForNavigation<PropertiesPage, PropertiesPageViewModel>();
-containerRegistry.RegisterForNavigation<PropertyPage, PropertyPageViewModel>(); containerRegistry.RegisterForNavigation<ContractsPage, ContractsPageViewModel>();
+            containerRegistry.RegisterForNavigation<PropertyPage, PropertyPageViewModel>(); containerRegistry.RegisterForNavigation<ContractsPage, ContractsPageViewModel>();
             containerRegistry.RegisterForNavigation<ContractPage, ContractPageViewModel>();
             containerRegistry.RegisterForNavigation<PropertyTabbedPage, PropertyTabbedPageViewModel>();
             containerRegistry.RegisterForNavigation<LeasingMasterDetailPage, LeasingMasterDetailPageViewModel>();
--- MyLeasing.Prism/MyLeasing.Prism/Views/MapPage.xaml.cs
+++ MyLeasing.Prism/MyLeasing.Prism/Views/MapPage.xaml.cs
@@ -1,12 +1,32 @@
-﻿using Xamarin.Forms;
+﻿using MyLeasing.Common.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace MyLeasing.Prism.Views
 {
     public partial class MapPage : ContentPage
     {
-        public MapPage()
+        private readonly IGeolocatorService _geolocatorService;
+
+        public MapPage(IGeolocatorService geolocatorService)
         {
             InitializeComponent();
+            _geolocatorService = geolocatorService;
+            MoveMapToCurrentPositionAsync();
+        }
+
+        private async void MoveMapToCurrentPositionAsync()
+        {
+            await _geolocatorService.GetLocationAsync();
+            if (_geolocatorService.Latitude != 0 && _geolocatorService.Longitude != 0)
+            {
+                var position = new Position(
+                    _geolocatorService.Latitude,
+                    _geolocatorService.Longitude);
+                MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
+                    position,
+                    Distance.FromKilometers(.5)));
+            }
         }
     }
 }
